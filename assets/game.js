ceaee7d72873819466e8b70153a537e71325a87f
Callymags <callanmaguire18@gmail.com>
2021-06-12 07:37:11 +0000

    feat: Add flash feature to selected button in sequence

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,9 @@
 var buttonDirections = ["up", "down", "left", "right", "buzz"]
 var gamePattern = [];
 
-function nextSequence() {
+function nextSequence(randomDirection) {
    var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
    gamePattern.push(randomDirection);
+
+   $("#" + randomDirection).fadeOut(100).fadeIn(100);
 }
