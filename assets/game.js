c139ef9902aa3b9d0551ca869975d0b3f46e5694
Callymags <callanmaguire18@gmail.com>
2021-06-15 18:40:24 +0000

    fix: Update nextSequence function with wrong direction variable

--- assets/game.js
+++ assets/game.js
@@ -5,14 +5,18 @@ function nextSequence(randomDirection) {
    var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
    gamePattern.push(randomDirection);
 
-   $("#" + randomDirection).fadeOut(100).fadeIn(100);
+   var audio = new Audio("assets/sounds/" + randomDirection + ".mp3")
+   audio.play();
+
+
+   var wrongDirection = buttonDirections[Math.floor(Math.random() * 5)];
+   $("#" + wrongDirection).fadeOut(100).fadeIn(100);
 
 
 /*    Audio javascript contribution from stackoverflow. See ReadMe Contributions section 
       for more information (https://stackoverflow.com/questions/9419263/how-to-play-audio) */
 
-   var audio = new Audio("assets/sounds/" + randomDirection + ".mp3")
-   audio.play();
+   
 }
 
 
