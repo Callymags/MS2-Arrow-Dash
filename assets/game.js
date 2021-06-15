0eabbb142d0a8a6d016477c41b34970c01e1be5f
Callymags <callanmaguire18@gmail.com>
2021-06-15 19:07:59 +0000

    feat: Add eventlisteners and userPattern variable

--- assets/game.js
+++ assets/game.js
@@ -1,9 +1,13 @@
 var buttonDirections = ["up", "down", "left", "right", "buzz"]
 var gamePattern = [];
+var userPattern = [];
 
 function nextSequence(randomDirection) {
    var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
    gamePattern.push(randomDirection);
+
+   /*    Audio javascript contribution from stackoverflow. See ReadMe Contributions section 
+      for more information (https://stackoverflow.com/questions/9419263/how-to-play-audio) */
 
    var audio = new Audio("assets/sounds/" + randomDirection + ".mp3")
    audio.play();
@@ -11,13 +15,14 @@ function nextSequence(randomDirection) {
 
    var wrongDirection = buttonDirections[Math.floor(Math.random() * 5)];
    $("#" + wrongDirection).fadeOut(100).fadeIn(100);
-
-
-/*    Audio javascript contribution from stackoverflow. See ReadMe Contributions section 
-      for more information (https://stackoverflow.com/questions/9419263/how-to-play-audio) */
-
-   
+ 
 }
+
+$(".game-buttons").click(function() {
+  var userDirection = $(this).attr("id");
+  userPattern.push(userDirection);
+  console.log(userPattern);
+});
 
 
 
