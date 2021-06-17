5d6b8d460c4670a27755fd24e98f82e40307c728
Callymags <callanmaguire18@gmail.com>
2021-06-17 10:51:30 +0000

    feat: Add button to start game

--- assets/game.js
+++ assets/game.js
@@ -1,43 +1,40 @@
 var buttonDirections = ["up", "down", "left", "right", "buzz"]
 var gamePattern = [];
 var userPattern = [];
+/*    Start document on click function below was taken from stackoverflow. See ReadMe Contributions section 
+      for more information (https://stackoverflow.com/questions/9336700/target-only-the-first-click-in-jquery) */
 var started = false
 
- /*    Start document on click function below was taken from stackoverflow. See ReadMe Contributions section 
-      for more information (https://stackoverflow.com/questions/9336700/target-only-the-first-click-in-jquery) */
-
-$(document).click(function(){
-    if(!started) {
-        nextSequence();
-        started = true;
-    }
+$(".start-button").click(function () {
+    $(this).hide();
+    started = true;
+    userPattern = [];
+    nextSequence();
 });
 
 function nextSequence(randomDirection) {
-   var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
-   gamePattern.push(randomDirection);
+    var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
+    gamePattern.push(randomDirection);
 
-   /*    Audio javascript contribution from stackoverflow. See ReadMe Contributions section 
-      for more information (https://stackoverflow.com/questions/9419263/how-to-play-audio) */
+    /*    Audio javascript contribution from stackoverflow. See ReadMe Contributions section 
+       for more information (https://stackoverflow.com/questions/9419263/how-to-play-audio) */
 
-   var audio = new Audio("assets/sounds/" + randomDirection + ".mp3")
-   audio.play();
+    var audio = new Audio("assets/sounds/" + randomDirection + ".mp3")
+    audio.play();
 
 
-   var wrongDirection = buttonDirections[Math.floor(Math.random() * 5)];
-   $("#" + wrongDirection).fadeOut(100).fadeIn(100);
- 
+    var wrongDirection = buttonDirections[Math.floor(Math.random() * 5)];
+    $("#" + wrongDirection).fadeOut(100).fadeIn(100);
+
 }
 
-$(".game-buttons").click(function() {
-  var userDirection = $(this).attr("id");
-  userPattern.push(userDirection);
+$(".game-buttons").click(function () {
+        var userDirection = $(this).attr("id");
+        userPattern.push(userDirection);
 
-  var clickAudio = new Audio("assets/sounds/" + userDirection + ".mp3")
-  clickAudio.play();
+        var clickAudio = new Audio("assets/sounds/" + userDirection + ".mp3")
+        clickAudio.play();
 
-  console.log(userPattern);
+        console.log(userPattern);
+
 });
-
-
-
