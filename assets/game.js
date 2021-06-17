be936910813b723b2f0be6a32d3a56d3321aa46e
Callymags <callanmaguire18@gmail.com>
2021-06-17 08:55:00 +0000

    feat: Add jquery function to start game sequence on first click

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,17 @@
 var buttonDirections = ["up", "down", "left", "right", "buzz"]
 var gamePattern = [];
 var userPattern = [];
+var started = false
+
+ /*    Start document on click function below was taken from stackoverflow. See ReadMe Contributions section 
+      for more information (https://stackoverflow.com/questions/9336700/target-only-the-first-click-in-jquery) */
+
+$(document).click(function(){
+    if(!started) {
+        nextSequence();
+        started = true;
+    }
+});
 
 function nextSequence(randomDirection) {
    var randomDirection = buttonDirections[Math.floor(Math.random() * 5)];
@@ -21,6 +32,10 @@ function nextSequence(randomDirection) {
 $(".game-buttons").click(function() {
   var userDirection = $(this).attr("id");
   userPattern.push(userDirection);
+
+  var clickAudio = new Audio("assets/sounds/" + userDirection + ".mp3")
+  clickAudio.play();
+
   console.log(userPattern);
 });
 
