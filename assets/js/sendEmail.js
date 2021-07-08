6a8244919ce7c5107add25451363e8999475e25f
Callymags <callanmaguire18@gmail.com>
2021-07-08 09:45:42 +0000

    fix: Update html and js code so form validates correctly

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,6 @@
+/*
+Sends email using emailjs and uses modals to display the message to the user
+*/
 function sendMail(contactForm) {
     emailjs.send("service_klkawbf","template_savnrya", {
         "from_name": contactForm.firstname.value, 
@@ -7,11 +10,22 @@ function sendMail(contactForm) {
     .then (
         function(response) {
             $("#alertModal").modal("show");
+            // Resets inputs of form if message is sent successfully
+            contactForm.reset();
         }, 
         function(error) {
             $("#alertfailModal").modal("show");
         }
-    );
-
+    )
+    // Manually close the contact-us modal
+    $("#contactModal").modal("hide"); 
+    // Stops page from refreshing
     return false;
 }
+
+$(document).ready(function(){
+    $("#contact-us-form").on("submit", function(e){
+        e.preventDefault(); // Stops page from refreshing
+        sendMail(this);
+    });
+});
